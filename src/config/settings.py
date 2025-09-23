3856d0a3548bbb620461bc68a0d318b2372fe881
meedoomostafa <mezo.225577@gmail.com>
2025-09-23 17:29:10 +0300

    Settings , README.md Added

---
+++ src/config/settings.py
@@ -0,0 +1,16 @@
+# YouTube API
+YOUTUBE_API_KEY="your_youtube_api_key"
+
+# Udemy API
+UDEMY_CLIENT_ID="your_client_id"
+UDEMY_CLIENT_SECRET="your_client_secret"
+
+# Google Books / Open Library (لو هتجيب كتب)
+GOOGLE_BOOKS_API_KEY="your_google_books_key"
+
+# SQL Server connection
+DB_CONNECTION_STRING=""
+
+# أي إعدادات إضافية
+MAX_FETCH_RESULTS=10
+DEFAULT_LANGUAGE="en"
