cce1bc9cefa354b3dbb280242a5b79eda353fdac
meedoomostafa <mezo.225577@gmail.com>
2025-10-31 01:31:06 +0200

    Classifier Supported

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,16 +1,24 @@
-# YouTube API
+################## Platforms ###############
+# Youtube
 YOUTUBE_API_KEY="your_youtube_api_key"
 
-# Udemy API
+#Udemy
 UDEMY_CLIENT_ID="your_client_id"
 UDEMY_CLIENT_SECRET="your_client_secret"
 
-# Google Books / Open Library (لو هتجيب كتب)
+#Google
 GOOGLE_BOOKS_API_KEY="your_google_books_key"
 
+##################################################
+
+################ HUGFace Models ##################
+
+# Hugging Face API
+HF_TOKEN = "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"
+
+#################################################
 # SQL Server connection
 DB_CONNECTION_STRING=""
 
-# أي إعدادات إضافية
 MAX_FETCH_RESULTS=10
 DEFAULT_LANGUAGE="en"
